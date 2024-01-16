d7d56652f919253041f7b9bcbd4161c10c303108
eggSys <egg.rkt2@gmail.com>
2024-01-17 00:28:37 +0700

    Initial Commit

---
+++ IOS Learn101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS Learn101
+//
+//  Created by Rungkrit Titachotnimit on 17/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS Learn101/IOS_Learn101App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Learn101App.swift
+//  IOS Learn101
+//
+//  Created by Rungkrit Titachotnimit on 17/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Learn101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
